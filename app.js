b99af803693332abf811b10255cb3b9b192fd0df
lucas <lucaspresipinheiro@gmail.com>
2023-07-26 16:45:56 -0300

    commit6

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const http = require("http");
 const port = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
+const puppeteer = require("puppeteer");
 
 app.use(express.json());
 app.use(
@@ -13,97 +14,101 @@ app.use(
   })
 );
 
-const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: { headless: true },
-});
+(async () => {
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
 
-client.initialize();
+  const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { headless: false, browser },
+  });
 
-client.on("qr", (qr) => {
-  console.log("bot-vita QRCode recebido", qr);
-});
+  client.initialize();
 
-client.on("authenticated", () => {
-  console.log("bot-vita Autenticado");
-});
+  client.on("qr", (qr) => {
+    console.log("bot-vita QRCode recebido", qr);
+  });
 
-client.on("auth_failure", (msg) => {
-  console.error("bot-vita Falha na autenticação", msg);
-});
+  client.on("authenticated", () => {
+    console.log("bot-vita Autenticado");
+  });
 
-client.on("ready", () => {
-  console.log("bot-vita Dispositivo pronto");
-});
+  client.on("auth_failure", (msg) => {
+    console.error("bot-vita Falha na autenticação", msg);
+  });
 
-client.on("change_state", (state) => {
-  console.log("bot-vita Status de conexão: ", state);
-});
+  client.on("ready", () => {
+    console.log("bot-vita Dispositivo pronto");
+  });
 
-client.on("disconnected", (reason) => {
-  console.log("bot-vita Cliente desconectado", reason);
-});
+  client.on("change_state", (state) => {
+    console.log("bot-vita Status de conexão: ", state);
+  });
 
-app.post(
-  "/send-message",
-  [body("number").notEmpty(), body("message").notEmpty()],
-  async (req, res) => {
-    const errors = validationResult(req).formatWith(({ msg }) => {
-      return msg;
-    });
+  client.on("disconnected", (reason) => {
+    console.log("bot-vita Cliente desconectado", reason);
+  });
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        status: false,
-        message: errors.mapped(),
+  app.post(
+    "/send-message",
+    [body("number").notEmpty(), body("message").notEmpty()],
+    async (req, res) => {
+      const errors = validationResult(req).formatWith(({ msg }) => {
+        return msg;
       });
-    }
 
-    const number = req.body.number;
-    const numberDDD = number.substr(0, 2);
-    const numberUser = number.substr(-8, 8);
-    const message = req.body.message;
-
-    if (numberDDD <= 30) {
-      const numberZDG = "55" + numberDDD + "9" + numberUser + "@c.us";
-      client
-        .sendMessage(numberZDG, message)
-        .then((response) => {
-          res.status(200).json({
-            status: true,
-            message: "bot-vita Mensagem enviada",
-            response: response,
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({
-            status: false,
-            message: "bot-vita Mensagem não enviada",
-            response: err.text,
-          });
+      if (!errors.isEmpty()) {
+        return res.status(422).json({
+          status: false,
+          message: errors.mapped(),
         });
-    } else if (numberDDD > 30) {
-      const numberZDG = "55" + numberDDD + numberUser + "@c.us";
-      client
-        .sendMessage(numberZDG, message)
-        .then((response) => {
-          res.status(200).json({
-            status: true,
-            message: "bot-vita Mensagem enviada",
-            response: response,
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({
-            status: false,
-            message: "bot-vita Mensagem não enviada",
-            response: err.text,
-          });
-        });
-    }
-  }
-);
+      }
 
-server.listen(port, function () {
-  console.log("Aplicação bot-vita rodando na porta *: " + port);
-});
+      const number = req.body.number;
+      const numberDDD = number.substr(0, 2);
+      const numberUser = number.substr(-8, 8);
+      const message = req.body.message;
+
+      if (numberDDD <= 30) {
+        const numberZDG = "55" + numberDDD + "9" + numberUser + "@c.us";
+        client
+          .sendMessage(numberZDG, message)
+          .then((response) => {
+            res.status(200).json({
+              status: true,
+              message: "bot-vita Mensagem enviada",
+              response: response,
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              status: false,
+              message: "bot-vita Mensagem não enviada",
+              response: err.text,
+            });
+          });
+      } else if (numberDDD > 30) {
+        const numberZDG = "55" + numberDDD + numberUser + "@c.us";
+        client
+          .sendMessage(numberZDG, message)
+          .then((response) => {
+            res.status(200).json({
+              status: true,
+              message: "bot-vita Mensagem enviada",
+              response: response,
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              status: false,
+              message: "bot-vita Mensagem não enviada",
+              response: err.text,
+            });
+          });
+      }
+    }
+  );
+
+  server.listen(port, function () {
+    console.log("Aplicação bot-vita rodando na porta *: " + port);
+  });
+})();
